8a888f557e447cd898f7aa6af75344fa65943127
long-crypto <175532236+long-crypto@users.noreply.github.com>
2025-02-01 01:53:09 +0000

    disable log

--- lua/kickstart/plugins/telescope.lua
+++ lua/kickstart/plugins/telescope.lua
@@ -28,7 +28,7 @@ return {
       { 'nvim-telescope/telescope-ui-select.nvim' },
 
       -- Useful for getting pretty icons, but requires a Nerd Font.
-      { 'nvim-tree/nvim-web-devicons', enabled = vim.g.have_nerd_font },
+      { 'nvim-tree/nvim-web-devicons',            enabled = vim.g.have_nerd_font },
     },
     config = function()
       -- Telescope is a fuzzy finder that comes with a lot of different things that
@@ -61,6 +61,7 @@ return {
         --     i = { ['<c-enter>'] = 'to_fuzzy_refine' },
         --   },
         -- },
+        history = false,
         -- pickers = {}
         extensions = {
           ['ui-select'] = {
