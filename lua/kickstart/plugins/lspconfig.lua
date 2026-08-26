b29e4e2713717fb863a20630d6513402652a7b95
long-crypto <175532236+long-crypto@users.noreply.github.com>
2026-08-26 08:30:10 +0000

    minichange

--- lua/kickstart/plugins/lspconfig.lua
+++ lua/kickstart/plugins/lspconfig.lua
@@ -182,7 +182,7 @@ require('mason-lspconfig').setup {
 -- You can press `g?` for help in this menu.
 local ensure_installed = vim.tbl_keys(servers or {})
 vim.list_extend(ensure_installed, {
--- You can add other tools here that you want Mason to install
+  -- You can add other tools here that you want Mason to install
 })
 
 require('mason-tool-installer').setup { ensure_installed = ensure_installed }
