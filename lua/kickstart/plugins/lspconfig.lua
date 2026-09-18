d48046721db86385dbebeb2641d9fdca259f4afe
long-crypto <175532236+long-crypto@users.noreply.github.com>
2026-09-18 08:52:42 +0000

    vim lsp log level

--- lua/kickstart/plugins/lspconfig.lua
+++ lua/kickstart/plugins/lspconfig.lua
@@ -35,7 +35,7 @@ require('fidget').setup {
   },
 }
 
-vim.lsp.set_log_level('OFF')
+vim.lsp.log.set_level('OFF')
 
 --  This function gets run when an LSP attaches to a particular buffer.
 --    That is to say, every time a new file is opened that is associated with
